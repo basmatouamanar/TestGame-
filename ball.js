0afc5221d1db446a55a2e812a82927bb5b69b608
btoumana <basmatoumanar55@gmail.com>
2026-01-01 14:26:00 +0100

    he

--- ball.js
+++ ball.js
@@ -10,18 +10,41 @@ let aWidth = a.offsetWidth
 let pabbleY = bHeight - 50
 let pabbleX = bdWidth / 2 - (a.offsetWidth) / 2
 a.style.transform = `translate(${pabbleX}px, ${pabbleY}px)`
+let keyword = {
+    left: false,
+    right: false
+}
 document.addEventListener("keydown", (e) => {
     if (e.key === "ArrowRight") {
-        pabbleX = pabbleX + 50
-        a.style.transform = `translate(${pabbleX}px,  ${pabbleY}px)`
-        if (pabbleX > bdWidth - a.offsetWidth) pabbleX = bdWidth - a.offsetWidth
+        keyword.right = true
+
     } else if (e.key === "ArrowLeft") {
-        pabbleX = pabbleX - 50
-        a.style.transform = `translate(${pabbleX}px,  ${pabbleY}px)`
-        if (pabbleX < 0) pabbleX = 0
-        
+        keyword.left = true
+
     }
 })
+document.addEventListener("keyup", (e) => {
+    if (e.key === "ArrowRight") {
+        keyword.right = false
+
+    }
+    if (e.key === "ArrowLeft") {
+        keyword.left = false
+    }
+})
+
+function movePabble() {
+
+    if (keyword.right) {
+        pabbleX = pabbleX + 5
+    }
+    if (keyword.left) {
+        pabbleX = pabbleX - 5
+    }
+    if (pabbleX > bdWidth - a.offsetWidth) pabbleX = bdWidth - a.offsetWidth
+    if (pabbleX < 0) pabbleX = 0
+    a.style.transform = `translate(${pabbleX}px,  ${pabbleY}px)`
+}
 
 let element = document.getElementById("balle")
 let board = document.getElementById("board")
@@ -31,27 +54,36 @@ let letterWidth = element.offsetWidth
 let letterHeight = element.offsetHeight
 let x = boardWidth / 2 - letterWidth / 2
 let y = boardHeight / 2 - letterHeight / 2
-let dx = 3
-let dy = 3
+let dx = 5
+let dy = 5
+
+
 
 function ball() {
     x = x + dx
     y = y + dy
     if (x <= 0 || (x + letterWidth) >= boardWidth) {
-        dx = -dx 
+        dx = -dx
     }
-    if ((y + letterHeight) >= boardHeight) {
-        y = boardHeight - letterHeight
-        return 
-    }
+    /* if ((y + letterHeight) >= boardHeight) {
+         y = boardHeight - letterHeight
+         return 
+     }*/
     if (y <= 0 || (y + letterHeight) >= boardHeight) {
-        dy = -dy 
+        dy = -dy
     }
-    if ((x <= (pabbleX + aWidth) && x >= pabbleX) && (y >= aHeight+pabbleY))  {
-        dy = -dy 
+    //let ydown = y + letterHeight  
+    let ydown = y
+    if (ydown >= pabbleY && y <= pabbleY + aHeight && x + letterWidth >= pabbleX && x <= pabbleX + aWidth) {
+        dy = -dy  
+
+        let contact = (x + letterWidth / 2 - pabbleX) / aWidth
+        let maxDX = 5  
+        dx = (contact - 0.5) * 2 * maxDX
     }
+
     element.style.transform = `translate(${x}px, ${y}px)`
-    
+    movePabble()
     requestAnimationFrame(ball)
 }
 requestAnimationFrame(ball)
